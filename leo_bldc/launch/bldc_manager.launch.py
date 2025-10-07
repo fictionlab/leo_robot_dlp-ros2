86ce9c23d6256dea9862506c969e12041b564c14
Aleksander Szymański <bitterisland6@gmail.com>
2025-10-07 17:08:56 +0200

    leo_bldc: bldc_manager.launch.py: adjust for single param file
    Signed-off-by: Aleksander Szymański <bitterisland6@gmail.com>

--- leo_bldc/launch/bldc_manager.launch.py
+++ leo_bldc/launch/bldc_manager.launch.py
@@ -2,27 +2,16 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, OpaqueFunction, TimerAction
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-from ament_index_python.packages import get_package_share_directory
-import os
+
 
 def launch_setup(context, *args, **kwargs):
-    mecanum_wheels = LaunchConfiguration("mecanum_wheels").perform(context)
-
-    bringup_share_dir = get_package_share_directory("leo_bringup")
-    diff_drive_params = os.path.join(bringup_share_dir, "config", "bldc_manager_diff_drive_params.yaml")
-    mecanum_params   = os.path.join(bringup_share_dir, "config", "bldc_manager_mecanum_params.yaml")
-    common_params    = os.path.join(bringup_share_dir, "config", "bldc_manager_common_params.yaml")
-
-    controller_params_file = mecanum_params if mecanum_wheels.lower() == "true" else diff_drive_params
-
     bldc_manager_node = Node(
         package="leo_bldc",
         executable="bldc_manager",
         parameters=[
             {"tf_frame_prefix": LaunchConfiguration("tf_frame_prefix")},
             {"mecanum_wheels": LaunchConfiguration("mecanum_wheels")},
-            common_params,
-            controller_params_file
+            LaunchConfiguration("params_file")
         ],
         output="screen"
     )
@@ -42,8 +31,15 @@ def generate_launch_description():
         description="The prefix added to each published TF frame id"
     )
 
+    params_file_arg = DeclareLaunchArgument(
+        "params_file",
+        default_value="",
+        description="Full path to the YAML parameter file for bldc_manager"
+    )
+
     return LaunchDescription([
         mecanum_wheels_arg,
         tf_frame_prefix_arg,
+        params_file_arg,
         OpaqueFunction(function=launch_setup)
     ])
